04d8fdb94b529329bbc76ac034d09c159a9848fc
Sivaram Pandariganthan <sivarampg95@gmail.com>
2020-05-24 22:03:55 +0530

    update url

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -3,7 +3,7 @@ let Urls = [];
 // Create an array of urls to fetch.
 for (var i = 1; i <= 88; i++) {
   if (i !== 17) {
-    Urls.push(`https://swapi.co/api/people/${i}/`);
+    Urls.push(`https://swapi.dev/api/people/${i}/`);
   }
 }
 
