aebe15e310dd6b05fdeebf9125501d361afffaa9
Sivaram Pandariganthan <sivarampg95@gmail.com>
2020-05-24 22:17:17 +0530

    reduce character count

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -1,7 +1,7 @@
 let Urls = [];
 
 // Create an array of urls to fetch.
-for (var i = 1; i <= 88; i++) {
+for (var i = 1; i <= 80; i++) {
   if (i !== 17) {
     Urls.push(`https://swapi.dev/api/people/${i}/`);
   }
